1e8f2527d6cc0f899a07ef69acfde13f334d9b95
rahmor <rahmor@yahoo.com>
2019-04-28 14:09:35 -0400

    breakdown techspecs further

--- src/TechSpecs/TechSpecs.js
+++ src/TechSpecs/TechSpecs.js
@@ -1,31 +1,22 @@
 import React from 'react';
-import TechSpecChoices from './TechSpecChoices';
+import TechSpecList from './TechSpecList';
 
 export default function TechSpecs(props) {
   const features = Object.keys(props.features).map(key => {
-    const options = props.features[key].map((item, index) => {
-      const selectedClass =
-        item.name === props.selected[key].name ? 'feature__selected' : '';
-      const featureClass = 'feature__option ' + selectedClass;
-      return (
-        <TechSpecChoices
-          key={index}
-          id={index}
-          tracker={key}
-          addClass={featureClass}
-          updateFeature={props.updateFeature}
-          item={item}
-        />
-      );
-    });
-
     return (
       <div className='feature' key={key}>
         <div className='feature__name'>{key}</div>
-        <ul className='feature__list'>{options}</ul>
+        <TechSpecList
+          selected={props.selected}
+          features={props.features}
+          key={key}
+          tracker={key}
+          updateFeature={props.updateFeature}
+        />
       </div>
     );
   });
+
   return (
     <section className='main__form'>
       <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
